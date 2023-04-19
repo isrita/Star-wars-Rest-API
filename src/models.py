f94dae0ff5c06527f32e18b8decf1ee5c6440d36
israel bonilla <israel.bonillad@gmail.com>
2023-04-19 08:12:35 +0000

    ko

--- src/models.py
+++ src/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     name = db.Column(db.String(120), unique=False, nullable=False)
+    favorite_people = db.relationship('FavoritePeople', backref = 'user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.name
@@ -19,11 +20,13 @@ class User(db.Model):
             "name" : self.name
             # do not serialize the password, its a security breach
         }
-""" class People(db.Model):
+class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     height = db.Column(db.String(120), unique=False, nullable=False)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    mass = db.column(db.string(80), unique=False, nullable=False)
+    mass = db.Column(db.String(80), unique=False, nullable=False)
+    favorite_people = db.relationship('FavoritePeople', backref= 'people', lazy=True)
+
 
 
     def serialize(self):
@@ -34,4 +37,19 @@ class User(db.Model):
             "mass" : self.mass
             # do not serialize the password, its a security breach
         }
- """
+
+class FavoritePeople (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "people_id": self.people_id,
+            "user_id": self.user_id,
+            "people_name": People.query.get(self.people_id).serialize()["name"],
+            "user_name": User.query.get(self.user_id).serialize()["name"],
+            "user": User.query.get(self.user_id).serialize(),
+            "people": People.query.get(self.people_id).serialize(),
+        }
