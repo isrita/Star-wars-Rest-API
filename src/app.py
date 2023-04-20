65fa4ecf397ac5b6fcb929b3bdd525856a6f8f31
israel bonilla <israel.bonillad@gmail.com>
2023-04-20 18:11:49 +0000

    ok

--- src/app.py
+++ src/app.py
@@ -8,22 +8,22 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, People, FavoritePeople, TokenBlockedlist
+from models import db, User, People, FavoritePeople, TokenBlockedList
 #from models import Person
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity, get_jwt
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
- 
+
 from datetime import date, time, datetime, timezone, timedelta
 
-from flask_bcrypt import Bcrypt #libreria para incriptaciones
+from flask_bcrypt import Bcrypt #librería para encriptaciones
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-#Inicio mi instancia de JWL
+#inicio de instancia de JWT
 app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_APP_KEY")  # Change this!
 jwt = JWTManager(app)
 
@@ -46,79 +46,96 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpo s
+# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
+def verificacionToken(jti):
+    jti#Identificador del JWT (es más corto)
+    print("jit", jti)
+    token = TokenBlockedList.query.filter_by(token=jti).first()
+
+    if token is None:
+        return False
+    
+    return True
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
-    users = User.query.all()
-    users = list(map(lambda item: item.serialize(), users)) 
+    users = User.query.all()  #<User Antonio>
+    users = list(map(lambda item: item.serialize(), users)) #{name:Antonio, password:123, ....} {name:Usuario2, password:123.... }
     print(users)
+  
     return jsonify(users), 200
 
 @app.route('/register', methods=['POST'])
 def register_user():
-    #recibir el body en json, des.jsonificarlos y almacenarlo en la variable body
-    body =  request.get_json()
+    #recibir el body en json, des-jsonificarlo y almacenarlo en la variable body
+    body = request.get_json() #request.json() pero hay que importar request y json
 
+    #ordernar cada uno de los campos recibidos
     email = body["email"]
     name = body["name"]
     password = body["password"]
     is_active = body["is_active"]
 
-    #validaciones 
+    #validaciones
     if body is None:
         raise APIException("You need to specify the request body as json object", status_code=400)
     if "email" not in body:
         raise APIException("You need to specify the email", status_code=400)
 
-    password_encrypted = bcrypt.generate_password_hash(password,10).decode('utf-8')
-    
-    #creada la clase user en la variable new_user
-    new_user = User(email=email, name=name, password=password, is_active=is_active)
+    password_encrypted = bcrypt.generate_password_hash(password,10).decode("utf-8")
 
+    #creada la clase User en la variable new_user
+    new_user = User(email=email, name=name, password=password_encrypted, is_active=is_active)
+
+    #comitear la sesión
     db.session.add(new_user) #agregamos el nuevo usuario a la base de datos
-    db.session.commit() #luego guardamos los cambios en la base de datos
-    
-    return jsonify({"mensaje":"usuario creado correctamente"}) , 201 
+    db.session.commit() #guardamos los cambios en la base de datos
 
-@app.route('/user/<int:id>', methods=['GET'])
+    return jsonify({"mensaje":"Usuario creado correctamente"}), 201 
+
+@app.route('/get-user/<int:id>', methods=['GET'])
 def get_specific_user(id):
-    user = User.query.get(id)
-
+    user = User.query.get(id)    
+  
     return jsonify(user.serialize()), 200
 
 @app.route('/get-user', methods=['POST'])
 def get_specific_user2():
-    body = request.get_json()
+    body = request.get_json()   
     id = body["id"]
-    user = User.query.get(id)
+
+    user = User.query.get(id)   
+  
     return jsonify(user.serialize()), 200
-   
+
 @app.route('/get-user', methods=['DELETE'])
 def delete_specific_user():
-    body = request.get_json()
+    body = request.get_json()   
     id = body["id"]
 
-    user = User.query.get(id)
+    user = User.query.get(id) 
 
     db.session.delete(user)
-    db.session.commit()
-    return jsonify("usuario borrado"), 200
+    db.session.commit()  
+  
+    return jsonify("Usuario borrado"), 200
 
 @app.route('/get-user', methods=['PUT'])
 def edit_user():
-    body = request.get_json()
+    body = request.get_json()   
     id = body["id"]
     name = body["name"]
 
-    user = User.query.get(id)
-    user.name = name #Modificar nombre de usuario
+    user = User.query.get(id)   
+    user.name = name #modifique el nombre del usuario
 
     db.session.commit()
-
+  
     return jsonify(user.serialize()), 200
 
 @app.route('/add-favorite/people', methods=['POST'])
@@ -129,16 +146,16 @@ def add_favorite_people():
 
     character = People.query.get(people_id)
     if not character:
-        raise APIException('Personaje no encontrado', status_code=404)
-
+        raise APIException('personaje no encontrado', status_code=404)
+    
     user = User.query.get(user_id)
     if not user:
-        raise APIException('Usuario no encontrado', status_code=404)
+        raise APIException('usuario no encontrado', status_code=404)
 
-    fav_exist = FavoritePeople.query.filter_by(user_id=user.id, people_id = character.id).first() is not None
-
+    fav_exist = FavoritePeople.query.filter_by(user_id = user.id, people_id = character.id).first() is not None
+    
     if fav_exist:
-        raise APIException('Usuario ya lo tiene agregado a Favoritos', status_code=404)
+        raise APIException('el usuario ya lo tiene agregado a favoritos', status_code=404)
 
     favorite_people = FavoritePeople(user_id=user.id, people_id=character.id)
     db.session.add(favorite_people)
@@ -146,25 +163,26 @@ def add_favorite_people():
 
     return jsonify(favorite_people.serialize()), 201
 
+
 @app.route('/favorites', methods=['POST'])
 def list_favorites():
     body = request.get_json()
     user_id = body["user_id"]
     if not user_id:
-        raise APIException('Faltan datos', status_code=404)
-
+        raise APIException('faltan datos', status_code=404)
+    
     user = User.query.get(user_id)
     if not user:
-        raise APIException('Usuario no encontrado', status_code=404)
+        raise APIException('usuario no encontrado', status_code=404)
 
-    user_favorites = FavoritePeople.query.filter_by(user_id=user.id).all() 
+    user_favorites = FavoritePeople.query.filter_by(user_id=user.id).all()    
     user_favorites_final = list(map(lambda item: item.serialize(), user_favorites))
 
-    #user_planets = FavoritePeople.query.filter_by(user_id=user.id).all() 
-    #user_planets_final = list(map(lambda item: item.serialize(), user_favorites))
+    #user_favorites_planets = FavoritePlanets.query.filter_by(user_id=user.id).all()
+    #user_favorites_final_planets = list(map(lambda item: item.serialize(), user_favorites_planets))
 
-    
-    #user_favorites_all = user_favorites_final + user_planets_final
+
+    #user_favorites_final = user_favorites_final + user_favorites_final_planets + user_favorites_final_vehicles
 
     return jsonify(user_favorites_final), 200
 
@@ -177,6 +195,9 @@ def login():
 
     if user is None:
         return jsonify({"message":"Login failed"}), 401
+    
+    """ if password != user.password:
+        return jsonify({"message":"Login failed"}), 401 """
 
     #validar el password encriptado
     if not bcrypt.check_password_hash(user.password, password):
@@ -184,7 +205,6 @@ def login():
     
     access_token = create_access_token(identity=user.id)
     return jsonify({"token":access_token}), 200
-
 
 @app.route("/protected", methods=["GET"])
 @jwt_required()
@@ -201,21 +221,22 @@ def protected():
     print("EL usuario es: ", user.name)
     return jsonify({"message":"Estás en una ruta protegida", "name": user.name}), 200
 
-@app.route("/logout", methods=["POST"])
+@app.route("/logout", methods=["GET"])
 @jwt_required()
 def logout():
-    jti = get_jwt()["jti"] #identificador de jwt
-    now = datetime.now(timezone.utc)
+    jti = get_jwt()["jti"] #Identificador del JWT (es más corto)
+    now = datetime.now(timezone.utc) 
 
     #identificamos al usuario
     current_user = get_jwt_identity()
     user = User.query.get(current_user)
 
-    tokenblocked = TokenBlockedlist(token=jti, created_at=now, email=user.email)
-    db.session.add(tokenblocked)
+    tokenBlocked = TokenBlockedList(token=jti , created_at=now, email=user.email)
+    db.session.add(tokenBlocked)
     db.session.commit()
 
     return jsonify({"message":"logout successfully"})
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     name = db.Column(db.String(120), unique=False, nullable=False)
     favorite_people = db.relationship('FavoritePeople', backref = 'user', lazy=True)
@@ -17,31 +17,32 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            "name" : self.name
+            "name": self.name
             # do not serialize the password, its a security breach
         }
+
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    height = db.Column(db.String(120), unique=False, nullable=False)
-    name = db.Column(db.String(80), unique=False, nullable=False)
+    height = db.Column(db.Float, unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=False, nullable=False)
     mass = db.Column(db.String(80), unique=False, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
     favorite_people = db.relationship('FavoritePeople', backref= 'people', lazy=True)
-
-
-
+    
     def serialize(self):
         return {
-            "id": self.id,
+            "id": self.id,        
+            "name": self.name,
             "height": self.height,
-            "name" : self.name,
-            "mass" : self.mass
+            "mass": self.mass,
+            "hair_color":self.hair_color
             # do not serialize the password, its a security breach
         }
-
+    
 class FavoritePeople (db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    people_id = db.Column(db.Integer, db.ForeignKey("people.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
 
     def serialize(self):
         return {
@@ -50,14 +51,13 @@ class FavoritePeople (db.Model):
             "user_id": self.user_id,
             "people_name": People.query.get(self.people_id).serialize()["name"],
             "user_name": User.query.get(self.user_id).serialize()["name"],
-            "user": User.query.get(self.user_id).serialize(),
-            "people": People.query.get(self.people_id).serialize(),
+            "user":User.query.get(self.user_id).serialize(),
+            "people":People.query.get(self.people_id).serialize()
         }
 
-    
-class TokenBlockedlist(db.Model):
+class TokenBlockedList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    toke = db.Column(db.String(250), unique=True, nullable=False)
+    token = db.Column(db.String(250), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
@@ -66,5 +66,5 @@ class TokenBlockedlist(db.Model):
             "id":self.id,
             "token":self.token,
             "email":self.email,
-            "created": self.created_at
-                }
+            "created":self.created_at
+        }
